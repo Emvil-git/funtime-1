a198232f0fa0dbbd38901f8a0503b1231c0f3043
Emvil-git <emvil542@gmail.com>
2025-04-17 23:25:26 +0800

    fix: add correct switch anims

--- brain.js
+++ brain.js
@@ -28,6 +28,9 @@ const contWidth = funCont.offsetWidth
 let grabLetter;
 let lastXPOS;
 let grabXPOS;
+let prevXPOS;
+let lastSwitchInd = null;
+let switchInd = null;
 
 const announceClick = (e) => {
     if (e.currentTarget.innerHTML !== " "){
@@ -119,21 +122,21 @@ const letterShifter = (grabPos, dropPos, letter) => {
         id: letter.id
     }
 
-    console.log(grabXPOS)
-    console.log(grabPos)
-    console.log(getGrabIndex(grabPos))
+    // console.log(grabXPOS)
+    // console.log(grabPos)
+    // console.log(getGrabIndex(grabPos))
     tLetters.splice(getGrabIndex(grabPos), 1); // "delete" the letter that is grabbed
-    console.log(tLetters)
+    // console.log(tLetters)
     tLetters.splice(getExpectedIndex(dropPos), 0, letterObj);
-    console.log(tLetters)
+    // console.log(tLetters)
 
     letters = tLetters;
 
-    console.log(letters)
+    // console.log(letters)
 }
 
 const posDetector = (e) => {
-    
+
     const rect = funCont.getBoundingClientRect();
     const currXPos = e.clientX - rect.left;
 
@@ -146,21 +149,43 @@ const posDetector = (e) => {
     if (grabLetter) {
         grabLetter.style.transform = `translateX(${dist * -1}px)`
         // console.log(`You will be at: ${getExpectedIndex(currXPos)}`)
-        console.log(toSwitch)
+        // console.log(toSwitch)
 
-        const switchInd = getCurrLetterIndex(toSwitch.id)
+        if (switchInd != lastSwitchInd) {
+            console.log('SWITCH INDEX CHANGE')
+            lastSwitchInd = switchInd;
+            switchInd = getCurrLetterIndex(toSwitch.id)
+        } else {
+            // lastSwitchInd = switchInd;
+            switchInd = getCurrLetterIndex(toSwitch.id)
+        }
+
+        // const switchInd = getCurrLetterIndex(toSwitch.id)
         const grabInd = getCurrLetterIndex(grabLetter.id)
 
         console.log(`Switch Index: ${switchInd}`)
+        console.log(`Last Switch Index: ${lastSwitchInd}`)
         console.log(`Grab Index: ${grabInd}`)
 
-        if (grabLetter.id !== toSwitch.id ) {
-            // toSwitch.style.transform = `translateX(44px)`
+        if (grabLetter.id !== toSwitch.id ) { // if switch index is not the same as the grab index
+            
             if (grabInd < switchInd) {
+                if (switchInd > lastSwitchInd) {
+                    funCont.children[lastSwitchInd].style.transform = "translateX(-44px)"
+                } else if (switchInd < lastSwitchInd){
+                    funCont.children[lastSwitchInd].style.transform = "translateX(0px)"
+                }
                 toSwitch.style.transform = `translateX(-44px)`
             } else {
+                if (switchInd > lastSwitchInd) {
+                    funCont.children[lastSwitchInd].style.transform = "translateX(0px)"
+                } //else if (switchInd < lastSwitchInd){
+                //     funCont.children[lastSwitchInd].style.transform = "translateX(-44px)"
+                // }
                 toSwitch.style.transform = `translateX(44px)`
             }
+        } else {
+            [...funCont.children].forEach(node => node.style.transform = "translateX(0)")
         }
     } else {
         // console.log(`no letter but your mouse is at: ${currXPos}`)
@@ -168,7 +193,7 @@ const posDetector = (e) => {
     }
 }
 
-funCont.addEventListener('mousemove', posDetector)
+document.addEventListener('mousemove', posDetector)
 
 const getCurrLetterIndex = (id) => {
     return letters.findIndex(lObj => lObj.id == id);
