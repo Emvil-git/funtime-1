4c10810d18ddcb910cacb41f307e31c7dce3ac22
Emvil-git <emvil542@gmail.com>
2025-04-17 14:45:36 +0800

    feat:add letter shifting functionality

--- brain.js
+++ brain.js
@@ -2,14 +2,26 @@ let letters = ['I', "'", 'M', ' ', 'J', 'U', 'S', 'T', ' ', 'H', 'A', 'V', 'I', 
 
 const funCont = document.getElementById("funtime-container");
 const contWidth = funCont.offsetWidth
-let funLetters = document.getElementsByClassName("funtime-letter");
+
 let grabLetter;
 let lastXPOS;
+let grabXPOS;
 
-console.log(contWidth)
-console.log(funLetters.length);
+const announceClick = (e) => {
+    if (e.currentTarget.innerHTML !== " "){
+        grabLetter = e.currentTarget;
+
+        const rect = funCont.getBoundingClientRect();
+        grabXPOS = e.clientX - rect.left;
+
+        console.log(`Grabbed letter ${e.currentTarget.innerHTML}`)
+        console.log(`Grab Index is: ${getGrabIndex(grabXPOS)}`)
+    }
+}
 
 const generateLetters = () => {
+    funCont.innerHTML = "";
+
     Array.from(letters).forEach(letter => {
         const letterNode = document.createElement("div")
         letterNode.innerHTML = letter
@@ -19,34 +31,76 @@ const generateLetters = () => {
         }
         funCont.appendChild(letterNode)
     })
+
+    let funLetters = document.getElementsByClassName("funtime-letter");
+
+    Array.from(funLetters).forEach(letter => {
+        letter.addEventListener('mousedown', announceClick)
+    })
 }
 
 document.onload = generateLetters();
 
-const announceClick = (e) => {
-    grabLetter = e.currentTarget;
-    console.log(`Grabbed letter ${e.currentTarget.innerHTML}`)
-}
 
 const announceUp = (e) => {
     if (grabLetter) {
+        const rect = funCont.getBoundingClientRect();
+        lastXPOS = e.clientX - rect.left;
+
+        console.log(`Last position of mouse is ${lastXPOS}`)
+        console.log(`expected index is: ${getExpectedIndex(lastXPOS)}`)
+
         console.log(`Let go of letter ${grabLetter.innerHTML}`)
+        console.log(`Grab X Pos is: ${grabXPOS}`)
+        letterShifter(grabXPOS, lastXPOS, grabLetter.innerHTML);
         grabLetter = ""
+        console.log(`Grabbed letter is now ${grabLetter}`);
+
+        generateLetters();
     } else {
         console.log('No letter grabbed')
     }
-
-    console.log(`Grabbed letter is now ${grabLetter}`)
 }
 
-Array.from(funLetters).forEach(letter => {
-    letter.addEventListener('mousedown', announceClick)
-    // letter.addEventListener('mouseup', announceUp)
-})
+
 
 document.addEventListener('mouseup', announceUp)
 
+const getExpectedIndex = (xPos) => {
+    if (xPos < 44) {
+        return 0;
+    } else if (xPos > 792) {
+        return 18;
+    } else {
+        let mult = Math.ceil(xPos/44);
+        return mult - 1
+    }
+}
 
+const getGrabIndex = (xPos) => {
+    if (xPos < 0) {
+        return;
+    } else if (xPos > 836) {
+        return;
+    } else {
+        let mult = Math.ceil(xPos/44);
+        return mult - 1;
+    }
+}
 
+const letterShifter = (grabPos, dropPos, letter) => {
+    let tLetters = letters.slice();
 
+    console.log(grabXPOS)
+    console.log(grabPos)
+    console.log(getGrabIndex(grabPos))
+    tLetters.splice(getGrabIndex(grabPos), 1); // "delete" the letter that is grabbed
+    console.log(tLetters)
+    tLetters.splice(getExpectedIndex(dropPos), 0, letter);
+    console.log(tLetters)
+
+    letters = tLetters;
+
+    console.log(letters)
+}
 
